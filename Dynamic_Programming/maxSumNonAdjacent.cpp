488d3313f8ee5d98919c63097913fdd3e7e8831f
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-03-18 07:55:45 +0530

    Add 18/03/2026

---
+++ Dynamic_Programming/maxSumNonAdjacent.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+// R -> L
+int recursively(vector<int> &nums, int n){
+    if(n<0) return 0;
+    if(n==0) return nums[0];
+
+    int incl=recursively(nums, n-2) + nums[n];
+    int excl=recursively(nums, n-1) + 0;
+
+    return max(incl, excl);
+}
+
+int memoization(vector<int> &nums, int n, vector<int> &dp){
+    if(n<0) return 0;
+    if(n==0) return nums[0];
+
+    if(dp[n]!=-1) return dp[n];
+
+    int incl=recursively(nums, n-2) + nums[n];
+    int excl=recursively(nums, n-1) + 0;
+
+    dp[n]= max(incl, excl);
+    return dp[n];
+}
+
+int tabulation(vector<int> &nums, int n){
+    vector<int> dp(n+1,0);
+
+    dp[0]=nums[0];
+
+    for(int i=1;i<=n;i++){
+        int incl = nums[i]; 
+        if(i - 2 >= 0) incl += dp[i-2];
+        int excl=dp[i-1]+0;
+
+        dp[i]=max(incl,excl);
+    }
+    return dp[n];
+}
+
+int spaceOptimization(vector<int> &nums, int n){
+    int prev2=0;
+    int prev1=nums[0];
+
+    for(int i=1;i<=n;i++){
+        int incl = prev2+nums[i];
+        int excl= prev1+0;
+
+        int ans=max(incl,excl);
+        prev2=prev1;
+        prev1=ans;
+    }
+    return prev1;
+}
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n=nums.size();
+    // vector<int> dp(n,-1);
+    // int ans=recursively(nums, n-1);
+    // int ans=memoization(nums, n-1, dp);
+    // int ans=tabulation(nums, n-1);
+    int ans=spaceOptimization(nums, n-1);
+    return ans;
+}
---
+++ Dynamic_Programming/minimumCoins.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int recursively(vector<int> &num, int x){
+    if(x==0) return 0;
+    if(x<0) return INT_MAX;
+
+    int mini=INT_MAX;
+
+    for(int i=0;i<num.size();i++){
+        int ans=recursively(num, x-num[i]);
+        if(ans!=INT_MAX){
+            // +1 for the newly added coin
+            mini=min(mini,ans+1);
+        }
+    }
+    return mini;
+}
+
+int memoization(vector<int> &num, int x, vector<int> &dp){
+    if(x==0) return 0;
+    if(x<0) return INT_MAX;
+
+    int mini=INT_MAX;
+
+    if(dp[x]!=-1) return dp[x];
+
+    for(int i=0;i<num.size();i++){
+        int ans=recursively(num, x-num[i]);
+        if(ans!=INT_MAX){
+            // +1 for the newly added coin
+            mini=min(mini,ans+1);
+        }
+    }
+    dp[x]=mini;
+    return mini;
+}
+
+int tabulation(vector<int> &num, int x){
+    vector<int> dp(x+1, INT_MAX);
+    dp[0]=0;
+
+    for(int i=1;i<=x;i++){
+        // check for each coin
+        for(int j=0;j<num.size();j++){
+            if(i-num[j]>=0 && dp[i-num[j]]!=INT_MAX){
+                dp[i]=min(dp[i],1+dp[i-num[j]]);
+            }
+        }
+    }
+
+    return dp[x];
+}
+
+int minimumElements(vector<int> &num, int x)
+{
+    // vector<int> dp(x+1,-1);
+    // int ans=recursively(num, x);
+    // int ans=memoization(num,x,dp);
+    int ans=tabulation(num,x);
+    // space optimization not possible
+    if(ans==INT_MAX) return -1;
+    return ans;
+}
