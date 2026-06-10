e4ed815a9dc4e513072ff41586a70c4871b10c62
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-06-10 18:41:48 +0530

    Check analyzer behaviour

--- testGo/test.go
+++ testGo/test.go
@@ -7,5 +7,7 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("This is a test program.")
+	// TODO
+	// Check custom analyzer
 	fmt.Print(fmt.Sprintf("Checking custom analyzer: %d", 100))
 }
