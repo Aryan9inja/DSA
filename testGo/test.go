1a7581575c3b66eaef0953ff8a8cd510863c601c
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-06-14 18:53:48 +0530

    Add LLM test4

--- testGo/test.go
+++ testGo/test.go
@@ -14,6 +14,15 @@ func main() {
 	// as it ignores ast issues and only outputs new issues
 
 	// Test after model fix
+
+	// Test for a bug
+	items := []int{10, 20, 30, 40, 50}
+	total := 0
+	for i := 0; i <= len(items); i++ {
+		total += items[i]
+	}
+	fmt.Printf("Total: %d", total)
+	
 }
 
 // Issues:
