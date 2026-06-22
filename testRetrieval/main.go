45e21e629f1893087dc405fc9d6649c5b2abfb10
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-06-22 21:35:55 +0530

    Test vector retrieval

---
+++ testRetrieval/main.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	values := []int{1, 2, 3, 4, 5}
+
+	// Checking if bug is detected
+	// And refrence is generated 
+	// Using pg vector
+	for i:=0; i<=len(values)+1; i++ {
+		fmt.Println(values[i])
+	}
+}
