f7c36a1239fcc3d422d446ae3804534a9e972a31
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-06-24 10:04:29 +0530

    Testing

---
+++ testing/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	// TODO: Enhance the program to accept user input and display a personalized greeting.
+
+	ints := []int{1, 2, 3, 4, 5}
+	for i := 0; i <= len(ints); i++ {
+		fmt.Println("Iteration:", ints[i])
+	}
+}
